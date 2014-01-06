dbf9e9e384df48b6dee15a2aaa773f44e3eb0c41
ray cutler <ray@vidvox.net>
2014-01-06 11:54:41 -0500

    updated project so it compiles universal binaries again

--- VVOSCStatic/VVOSCStatic.h
+++ VVOSCStatic/VVOSCStatic.h
@@ -6,21 +6,4 @@
 //
 //
 
-#import "OSCConstants.h"
-
-#import "OSCValue.h"
-#import "OSCMessage.h"
-#import "OSCBundle.h"
-#import "OSCPacket.h"
-
-#import "OSCOutPort.h"
-#import "OSCInPort.h"
-
-#import "OSCManager.h"
-#import "OSCZeroConfManager.h"
-
-#import "OSCNode.h"
-#import "OSCAddressSpace.h"
-#import "OSCQueryReply.h"
-
-#import "OSCStringAdditions.h"
+#import "VVOSC.h"
